8b48fb7a926b307aec446b05f96d7d2346040e81
Jessica C <jgcreiglow@gmail.com>
2018-03-28 10:35:50 -0400

    updated home and new public js files

--- public/assets/js/new.js
+++ public/assets/js/new.js
@@ -1,30 +1,26 @@
+// this one is going to be like the CMS in author joins
 
+$(document).ready(function() {
+  // Getting jQuery references to the Application Info
+  var bodyInput = $("#body");
+  var titleInput = $("#title");
+  var cmsForm = $("#cms");
+  var authorSelect = $("#author");
 
-// When user clicks add-btn
-$("#**BUTTON**ID**").on("click", function(event) {
-    event.preventDefault();
-  
-    // Make a newJob object
-    var newJob = {
-     Company: $("company").val().trim(),
-     Title:  $("#***ID***").val().trim(),
-    Location: $("#***ID***").val().trim(),
-    Contact_Email: $("#***ID***").val().trim()
-    };
-  
-    // Send an AJAX POST-request with jQuery
-    $.post("/api/new", newJob)
-      // On success, run the following code
-      .then(function(data) {
-        // Log the data we found
-        console.log(data);
-      });
-  
-    // Empty each input box by replacing the value with an empty string
-    $("#***ID***").val("");
-    $("#***ID***").val("");
-    $("#***ID***").val("");
-    $("#***ID***").val("");
-  
-  });
-  
+    // Adding an event listener for when the form is submitted
+    $(cmsForm).on("submit", handleFormSubmit);
+    // Gets the part of the url that comes after the "?" (which we have if we're updating a n application)
+    var url = window.location.search;
+
+  var jobId;
+  // Sets a flag for whether or not we're updating an app to be false initially
+  var updating = false;
+
+    // If we have this section in our url, we pull out the job  id from the url
+  if (url.indexOf("?job_id=") !== -1) {
+   jobId = url.split("=")[1];
+    getJobData(jobId, "job");
+  }
+
+    // Getting the Jobs
+    getJobs();
